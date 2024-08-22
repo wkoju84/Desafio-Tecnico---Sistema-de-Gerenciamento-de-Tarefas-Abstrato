fce33009946bebe4c34a88e3ec90facb6597163e
wkoju84 <wkoju84@gmail.com>
2024-08-22 12:12:06 -0300

    Adiciona classe Tarefa com contrutores e getters e setters

---
+++ src/main/java/com/desafio_tecnico/gerenciamento_de_tarefas/entities/Tarefa.java
@@ -0,0 +1,84 @@
+package com.desafio_tecnico.gerenciamento_de_tarefas.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private boolean realizado;
+
+    private int prioridade;
+
+    public Tarefa() {
+    }
+
+    public Tarefa(Long id, String nome, String descricao, boolean realizado, int prioridade) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    /*
+    git add src/main/java/com/example/projectname/model/Tarefa.java
+git commit -m "Add Tarefa class with constructors, getters, and setters
+
+- Created `Tarefa` class in `model` package.
+- Added private fields `id`, `titulo`, `descricao`, and `dataCriacao`.
+- Implemented default constructor and constructor with all fields.
+- Added getters and setters for each field."
+
+     */
+}
