46aede0c87931172d7cb630595cde217dbfd5009
wkoju84 <wkoju84@gmail.com>
2024-08-22 11:58:30 -0300

    Initial commit: Setup project structure

---
+++ src/main/java/com/desafio_tecnico/gerenciamento_de_tarefas/GerenciamentoDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.desafio_tecnico.gerenciamento_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio_tecnico/gerenciamento_de_tarefas/GerenciamentoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_tecnico.gerenciamento_de_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
