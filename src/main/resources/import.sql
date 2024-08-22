d97d7704c34cd679a8d40590516f39b8f6974611
wkoju84 <wkoju84@gmail.com>
2024-08-22 12:33:09 -0300

    Adiciona dados no banco H2, seeding de dados e import.sql na pasta resources

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tarefa(nome, descricao, realizado, prioridade) VALUES ('Comprar ração', 'Ir na loja do centro', FALSE, 1);
+INSERT INTO tarefa(nome, descricao, realizado, prioridade) VALUES ('Academia', 'Realizar treino de perna', TRUE, 2);
+INSERT INTO tarefa(nome, descricao, realizado, prioridade) VALUES ('Entregar TCC', 'Terminar descrição das atividades', FALSE, 3);
+INSERT INTO tarefa(nome, descricao, realizado, prioridade) VALUES ('Pagar conta', 'Ir na lotérica da Maria', TRUE, 3);
