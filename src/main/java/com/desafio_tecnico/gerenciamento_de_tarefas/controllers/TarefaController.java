4dff03a76ed2e3c1f98d3e89221456f877f00651
wkoju84 <wkoju84@gmail.com>
2024-08-25 17:40:09 -0300

    Adiciona método inserir tarefa na camada controllers

--- src/main/java/com/desafio_tecnico/gerenciamento_de_tarefas/controllers/TarefaController.java
+++ src/main/java/com/desafio_tecnico/gerenciamento_de_tarefas/controllers/TarefaController.java
@@ -4,7 +4,9 @@ import com.desafio_tecnico.gerenciamento_de_tarefas.dtos.TarefaDTO;
 import com.desafio_tecnico.gerenciamento_de_tarefas.services.TarefaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,5 +35,13 @@ public class TarefaController {
     public ResponseEntity<Void> excluirTarefa(@PathVariable Long id){
         tarefaService.excluir(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<TarefaDTO> inserirTarefa(@RequestBody TarefaDTO dto){
+        dto = tarefaService.inserir(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
