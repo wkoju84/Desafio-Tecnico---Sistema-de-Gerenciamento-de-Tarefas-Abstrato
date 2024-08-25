3a47147d309bed7c2bbc50e9c14c059eb19e40fe
wkoju84 <wkoju84@gmail.com>
2024-08-25 17:19:58 -0300

    Adiciona pacote Service
    Adicionando TarefaService e método buscar todos para listar todas as tarefas

---
+++ src/main/java/com/desafio_tecnico/gerenciamento_de_tarefas/services/TarefaService.java
@@ -0,0 +1,31 @@
+package com.desafio_tecnico.gerenciamento_de_tarefas.services;
+
+import com.desafio_tecnico.gerenciamento_de_tarefas.dtos.TarefaDTO;
+import com.desafio_tecnico.gerenciamento_de_tarefas.entities.Tarefa;
+import com.desafio_tecnico.gerenciamento_de_tarefas.repositories.TarefaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TarefaService {
+
+    private TarefaRepository tarefaRepository;
+
+    public TarefaService(TarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<TarefaDTO> buscarTodos() {
+        List<Tarefa> tarefas = tarefaRepository.findAll();
+        return tarefas.stream().sorted(Comparator.comparing(Tarefa::getPrioridade)
+                        .reversed() //Ordena as tarefas pela prioridade em ordem decrescente
+                        .thenComparing(Tarefa::getNome))//Ordena as tarefas por nome em ordem crescente
+                .map(TarefaDTO::new)//Converte as entidades
+                .collect(Collectors.toList());//Coleta os resultados em uma lista
+    }
+}
